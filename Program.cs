8a0c2281f65668cbd8f8fdf862dd64267700b95f
nelsh <nelsh@users.noreply.github.com>
2012-08-06 14:09:24 +0900

    add: reading filter file add: example filter file application.conf for local testing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using System.Text;
 using CommandLine;
@@ -15,9 +16,12 @@ namespace winlogcheck
 		static public Options options = new Options();
 		// struct for store program settings
 		static public ProgramSettings currentSettings;
+		// time line for all events over the past day
+		static string whereTime = "";
 
 		static void Main(string[] args)
 		{
+			Program.whereTime = DateTime.UtcNow.AddDays(-1).ToString("yyyyMMdd HH:mm:ss");
 			log.Info("WinLogCheck start");
 
 			// Read and check command line arguments
@@ -33,11 +37,13 @@ namespace winlogcheck
 				{
 					// Get specific report in specific eventlog
 					log.Info(String.Format("Mode: '{0}', filter: '{1}' in eventlog '{2}'", options.Mode, options.Filter, options.LogName));
+					getReportFilter(options.Mode, options.LogName, options.Filter);
 				}
 				else
 				{
 					// Get report in specific eventlog
 					log.Info(String.Format("Mode '{0}',  ALL filters in eventlog '{1}'", options.Mode, options.LogName));
+					getReportLog(options.Mode, options.LogName);
 				}
 			}
 			else
@@ -79,6 +85,99 @@ namespace winlogcheck
 				Environment.Exit(2);
 			}
 		}
+
+		static void getReportFilter(string mode, string eventlog, string filter)
+		{
+			ArrayList filters = readFilter(mode, eventlog, filter);
+			getEvents(mode, eventlog, filters);
+		}
+
+		static void getReportLog(string mode, string eventlog)
+		{
+			ArrayList filters = readFilter(mode, eventlog);
+			getEvents(mode, eventlog, filters);
+		}
+
+		static void getReport(string mode, string eventlog)
+		{
+		}
+
+		static ArrayList readFilter(string mode, string eventlog, string filter = "")
+		{
+			// array to store filters
+			ArrayList filters = new ArrayList();
+			// get filename for filters
+			string filterFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, mode, eventlog + ".conf");
+			Program.log.Debug(String.Format("Read filter file \"{0}\"", filterFile));
+			// if usign filter and file not found - exit
+			// else return empty filters
+			if (!File.Exists(filterFile))
+			{
+				if (!String.IsNullOrWhiteSpace(filter))
+				{
+					Program.log.Error(String.Format("WinLogCheck stop: Cannot read {0}. Nothing do.", filterFile));
+					Environment.Exit(2);
+				}
+				else
+				{
+					Program.log.Warn(String.Format("Cannot read {0}. Filters DISABLED.", filterFile));
+					return filters;
+				}
+
+			}
+			// read INI-file
+			// if usign filter and file not read - exit
+			// else return empty filters
+			IniParser.FileIniDataParser iniParser = new FileIniDataParser();
+			iniParser.KeyValueDelimiter = ':';
+			IniData iniData = null;
+			try
+			{
+				iniData = iniParser.LoadFile(filterFile);
+			}
+			catch (Exception ex)
+			{
+				if (!String.IsNullOrWhiteSpace(filter))
+				{
+					Program.log.Error(String.Format("WinLogCheck stop: Cannot read '{0}'. {1}", filterFile, ex));
+					Environment.Exit(2);
+				}
+				else
+				{
+					Program.log.Warn(String.Format("Cannot read {0}. Filters DISABLED. {1}", filterFile, ex));
+					return filters;
+				}
+			}
+			// if usign filter and it not read - exit
+			// else read all filters from file
+			if (!String.IsNullOrWhiteSpace(filter))
+			{
+				try
+				{
+					filters.Add(iniData["General"][filter]);
+					Program.log.Debug(String.Format("Use filter '{0}'", filter));
+				}
+				catch (Exception ex)
+				{
+					Program.log.Error(String.Format("WinLogCheck stop: Cannot read filter '{0}'. {1}", filter, ex));
+					Environment.Exit(2);
+				}
+			}
+			else
+			{
+				foreach (KeyData key in iniData.Sections["General"])
+				{
+					filters.Add(key.Value);
+					Program.log.Debug(String.Format("Use filter '{0}' = {1}", key.KeyName, key.Value));
+				}
+			}
+			return filters;
+		}
+
+		static void getEvents(string mode, string log, ArrayList filters)
+		{
+		}
+	
 	}
 
 	class Options
@@ -88,11 +187,11 @@ namespace winlogcheck
 		[Option("m", null, Required = true, HelpText = "Report mode")]
 		public string Mode { get; set; }
 
+		[Option("f","filter", HelpText = "Filter name")]
+		public string Filter { get; set; }
+
 		[OptionArray("l", null, HelpText = "Event log name")]
 		public string[] LogNameArray { get; set; }
-
-		[Option("f", null, HelpText = "Filter name")]
-		public string Filter { get; set; }
 
 		[HelpOption]
 		public string GetUsage()
@@ -106,7 +205,7 @@ namespace winlogcheck
 			usage.AppendLine("\t\tReport mode. Required");
 			usage.AppendLine("\t-l <logname>");
 			usage.AppendLine("\t\tEvent Log Name.");
-			usage.AppendLine("\t-r <filtername>");
+			usage.AppendLine("\t-f <filtername>");
 			usage.AppendLine("\t\tFilter name");
 			usage.AppendLine();
 			usage.AppendLine("Quick usage:");
@@ -161,10 +260,12 @@ namespace winlogcheck
 				ReportPath = iniData["General"]["ReportPath"];
 				if (String.IsNullOrWhiteSpace(ReportPath))
 					ReportPath = "reports";
+				Program.log.Info(String.Format("Use directory \"{0}\" for temporary report.", ReportPath));
 			}
 			catch 
 			{
 				ReportPath = "reports";
+				Program.log.Info(String.Format("Use default directory \"{0}\" for temporary report.", ReportPath));
 			}
 			if (!Directory.Exists(ReportPath))
 			{
